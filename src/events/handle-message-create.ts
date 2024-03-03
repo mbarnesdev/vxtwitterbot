468514a83ae3b6c4ab6f30aef17fa1e14ca822d5
mbarnesdev <martinbarnes.dev@gmail.com>
2024-03-03 13:34:18 +0000

    prettier formatting

--- src/events/handle-message-create.ts
+++ src/events/handle-message-create.ts
@@ -1,36 +1,33 @@
-import { Message } from "discord.js"
+import { Message } from "discord.js";
 
-const fixHostname = "vxtwitter.com"
+const fixHostname = "vxtwitter.com";
 
-const validHostnames = [
-    "x.com",
-    "twitter.com",
-]
+const validHostnames = ["x.com", "twitter.com"];
 
 function extractTwitterLinks(messageContent: string) {
-    const links = messageContent.match(/\bhttps?:\/\/\S+/gi) ?? [];
-    
-    return links
-        .filter((link) => {
-            const hostname = new URL(link).hostname
-            return validHostnames.includes(hostname)
-        })
-        .map((link) => {
-            const hostname = new URL(link).hostname
-            return link.replace(hostname, fixHostname)
-        })
+  const links = messageContent.match(/\bhttps?:\/\/\S+/gi) ?? [];
+
+  return links
+    .filter((link) => {
+      const hostname = new URL(link).hostname;
+      return validHostnames.includes(hostname);
+    })
+    .map((link) => {
+      const hostname = new URL(link).hostname;
+      return link.replace(hostname, fixHostname);
+    });
 }
 
 export function handleMessageCreate(message: Message) {
-    const isBot = message.author.bot
-    if (isBot) return
+  const isBot = message.author.bot;
+  if (isBot) return;
 
-    const links = extractTwitterLinks(message.content)
-    if (!links.length) return;
+  const links = extractTwitterLinks(message.content);
+  if (!links.length) return;
 
-    message.suppressEmbeds();
+  message.suppressEmbeds();
 
-    for (let i = 0; i < links.length; ++i) {
-        message.channel.send(links[i])
-    }
+  for (let i = 0; i < links.length; ++i) {
+    message.channel.send(links[i]);
+  }
 }
